9ba8376e615eb9a3535421eeafa7bf91e390112b
Evgeni Khoptiar <evgeni.khoptiar@emg-soft.com>
2021-04-15 23:09:44 +0300

    useStore fix

--- hooks/useStore.ts
+++ hooks/useStore.ts
@@ -1,12 +1,12 @@
-import {useContext} from 'react'
-import {Index, StoreContext} from '../stores';
+import { useContext } from "react";
+import { RootStore, StoreContext } from "../stores";
 
-export const useStore = (): Index => {
-    const store = useContext(StoreContext);
-    if (!store) {
-        throw new Error('useStore must be used within a StoreProvider.');
-    }
-    return store;
+export const useStore = (): RootStore => {
+  const store = useContext(StoreContext);
+  if (!store) {
+    throw new Error("useStore must be used within a StoreProvider.");
+  }
+  return store;
 };
 
-export default useStore
+export default useStore;
